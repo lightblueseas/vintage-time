8da42e6aa511932dcf5622475446fc0c100792cb
James Phelps <tyler.durden69@gmx.de>
2018-12-13 17:52:15 +0100

    Update DateExtensions.java

--- src/main/java/de/alpharogroup/date/DateExtensions.java
+++ src/main/java/de/alpharogroup/date/DateExtensions.java
@@ -29,9 +29,11 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.TimeZone;
 
 import lombok.experimental.UtilityClass;
